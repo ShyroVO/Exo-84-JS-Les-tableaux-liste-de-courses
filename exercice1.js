4af751023f5ce37a76071e81ac16c778e25a156e
ShyroVO <bosquet.laurie@outlook.com>
2020-12-15 10:33:32 +0100

    presque

--- exercice1.js
+++ exercice1.js
@@ -13,7 +13,6 @@ let articles = [
 // Votre code à partir d'ici :
 let liste = document.getElementById('listeCourse');
 
-let textAjout = document.getElementById('contenusAjout').toString();
 let boutonAjout = document.getElementById('ajouter');
 
 for (let i = 0; i < articles.length; i++) {
@@ -23,9 +22,8 @@ for (let i = 0; i < articles.length; i++) {
 }
 
 boutonAjout.addEventListener('click', function ajoutItem() {
+    let textAjout = document.getElementById('contenusAjout').value;
     articles.push(textAjout);
     console.log(articles);
+    return articles;
 })
-
-
-
