55cbff1499eddf4304a2e15b74fb9c2376401074
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-16 19:32:04 +0100

    ajout bouton

--- exercice1.js
+++ exercice1.js
@@ -17,17 +17,27 @@ let i = 0;
 
 for (i = 0; i < articles.length; i++) {
     let nouveau = document.createElement('li');
+    let masquer = document.createElement('button');
+
     nouveau.innerHTML = articles[i];
+    masquer.innerHTML = "Masquer";
+    masquer.id = "'" + 'button' + i + "'";
     liste.appendChild(nouveau);
+    nouveau.appendChild(masquer);
+
 }
 
 boutonAjout.addEventListener('click', function ajoutItem() {
     let textAjout = document.getElementById('contenusAjout').value;
     let nouveauItem = document.createElement('li');
+    let masquer = document.createElement('button');
+
     articles.push(textAjout);
     nouveauItem.innerHTML = articles[i];
+    masquer.innerHTML = "Masquer";
+    masquer.id = "'" + 'button' + i + "'";
     liste.appendChild(nouveauItem);
+    nouveauItem.appendChild(masquer);
     i++
 })
-
 
